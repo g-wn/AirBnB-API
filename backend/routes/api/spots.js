8a54a70926e19a52e2a0ab568b827dd639e98ac5
Gray Nance <graynance@gmail.com>
2022-10-25 09:38:49 -0700

    Fix eager loading by adding required: false to potentially empty includes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -70,9 +70,18 @@ router.get('/current', requireAuth, async (req, res, _next) => {
   const { user } = req;
 
   const currentUserSpots = await Spot.findAll({
-    where: {
-      ownerId: user.id
-    }
+    include: [
+      { model: Review, attributes: [], required: false },
+      { model: SpotImage, where: { preview: true }, attributes: [], required: false }
+    ],
+    attributes: {
+      include: [
+        [Sequelize.fn('ROUND', Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 1), 'avgRating'],
+        [Sequelize.col('SpotImages.url'), 'previewImage']
+      ]
+    },
+    group: ['Spot.id', 'previewImage'],
+    where: {ownerId: user.id}
   });
 
   if (currentUserSpots.length) {
@@ -88,9 +97,9 @@ router.get('/current', requireAuth, async (req, res, _next) => {
 router.get('/:spotId', async (req, res, _next) => {
   const spot = await Spot.findByPk(req.params.spotId, {
     include: [
-      { model: SpotImage, attributes: ['id', 'url', 'preview'] },
-      { model: User, as: 'Owner', attributes: ['id', 'firstName', 'lastName'] },
-      { model: Review, attributes: [] }
+      { model: SpotImage, attributes: ['id', 'url', 'preview'], required: false },
+      { model: User, as: 'Owner', attributes: ['id', 'firstName', 'lastName'], required: false },
+      { model: Review, attributes: [], required: false }
     ],
     attributes: {
       include: [
@@ -108,8 +117,8 @@ router.get('/:spotId', async (req, res, _next) => {
 router.get('/', async (_req, res, _next) => {
   const allSpots = await Spot.findAll({
     include: [
-      { model: Review, attributes: [] },
-      { model: SpotImage, where: { preview: true }, attributes: [] }
+      { model: Review, attributes: [], required: false },
+      { model: SpotImage, where: { preview: true }, attributes: [], required: false }
     ],
     attributes: {
       include: [
