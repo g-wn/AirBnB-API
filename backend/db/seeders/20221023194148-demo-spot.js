c3af74cac36e968be3ca1224f32053f7ad0df410
Gray Nance <graynance@gmail.com>
2022-10-25 07:48:57 -0700

    remove previewImage from seed data on Spots

--- backend/db/seeders/20221023194148-demo-spot.js
+++ backend/db/seeders/20221023194148-demo-spot.js
@@ -14,8 +14,7 @@ module.exports = {
         lng: -118.31352501176664,
         name: 'Test Name 1',
         description: 'Test Description 1',
-        price: 150.99,
-        previewImage: 'https://www.testpreviewimg.com/test1'
+        price: 150.99
       },
       {
         ownerId: 2,
@@ -27,8 +26,7 @@ module.exports = {
         lng: -118.3113699995813,
         name: 'Test Name 2',
         description: 'Test Description 2',
-        price: 250.99,
-        previewImage: 'https://www.testpreviewimg.com/test2'
+        price: 250.99
       },
       {
         ownerId: 3,
@@ -40,8 +38,7 @@ module.exports = {
         lng: -117.80254810567625,
         name: 'Test Name 3',
         description: 'Test Description 3',
-        price: 350.99,
-        previewImage: 'https://www.testpreviewimg.com/test3'
+        price: 350.99
       }
     ]);
   },
