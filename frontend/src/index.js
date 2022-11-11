e626a3094baf1a5b29c6324824ccb28d115a530f
Gray Nance <graynance@gmail.com>
2022-11-11 15:19:39 -0800

    LoginForm Modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './index.css';
 
-import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { ModalProvider } from './context/Modal';
 
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-
 import * as sessionActions from './store/session';
 
 const store = configureStore();
@@ -25,9 +25,11 @@ if (process.env.NODE_ENV !== 'production') {
 function Root() {
   return (
     <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </ReduxProvider>
   );
 }
