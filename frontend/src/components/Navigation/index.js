6ce4e273a8e8f3eabef0d7a21ebef9c64244d501
Gray Nance <graynance@gmail.com>
2022-11-15 08:10:27 -0800

    update spot

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,10 +5,10 @@ import ProfileButton from './ProfileButton';
 import './Navigation.css';
 import SignupFormModal from '../SignupFormModal';
 import LoginFormModal from '../LoginFormModal';
-import { postSpot } from '../../store/spots';
+import { postSpot, putSpot } from '../../store/spots';
 
 export default function Navigation({ isLoaded }) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
   let sessionLinks = <ProfileButton user={sessionUser} />;
 
@@ -25,8 +25,24 @@ export default function Navigation({ isLoaded }) {
       price: 1000
     };
 
-    return dispatch(postSpot(demoSpot))
-  }
+    return dispatch(postSpot(demoSpot));
+  };
+
+  const handleUpdateDemoSpot = e => {
+    e.preventDefault();
+
+    const updatedSpot = {
+      address: 'UPDATE',
+      city: 'UPDATE',
+      state: 'UPDATE',
+      country: 'TEST COUNTRY',
+      name: 'TEST NAME',
+      description: 'TEST DESCRIPTION',
+      price: 1000
+    };
+
+    return dispatch(putSpot(36, updatedSpot));
+  };
 
   return (
     <div className='nav-container'>
@@ -37,9 +53,8 @@ export default function Navigation({ isLoaded }) {
         <SignupFormModal />
         <LoginFormModal />
       </span>
-      {sessionUser && (
-        <button onClick={handleCreateDemoSpot}>CREATE A SPOT</button>
-      )}
+      {sessionUser && <button onClick={handleCreateDemoSpot}>CREATE A TEST SPOT</button>}
+      {sessionUser && <button onClick={handleUpdateDemoSpot}>UPDATE THE TEST SPOT</button>}
       {isLoaded && <span>{sessionLinks}</span>}
     </div>
   );
