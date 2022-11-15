827a9b59e42f05e3ee4554c1c00740b11ac7f5ec
Gray Nance <graynance@gmail.com>
2022-11-15 15:47:04 -0800

    create spot splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,8 @@ import * as spotsActions from './store/spots';
 import Navigation from './components/Navigation';
 import Filters from './components/Filters';
 import Splash from './components/Splash';
-import SpotDetail from './components/SpotDetail'
+import SpotDetail from './components/SpotDetail';
+import HostSplash from './components/HostSplash';
 
 function App() {
   const dispatch = useDispatch();
@@ -20,17 +21,27 @@ function App() {
   return (
     isLoaded && (
       <>
-        <Navigation isLoaded={isLoaded} />
         <Switch>
           <Route
             exact={true}
             path={`/`}
           >
+            <Navigation isLoaded={isLoaded} />
             <Filters />
             <Splash />
           </Route>
-          <Route path={`/spots/:spotId`}>
+          <Route
+            path={`/spots/:spotId`}
+            exact={true}
+          >
+            <Navigation isLoaded={isLoaded} />
             <SpotDetail />
+          </Route>
+          <Route
+            path={`/host`}
+            exact={true}
+          >
+            <HostSplash isLoaded={isLoaded} />
           </Route>
           <Route>Page Not Found</Route>
         </Switch>
