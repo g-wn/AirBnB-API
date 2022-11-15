d6ec9139bf11fedc5d0b594f4a6990488efc02d0
Gray Nance <graynance@gmail.com>
2022-11-14 21:32:21 -0800

    seed data - shorten reviews

--- backend/db/seeders/20221023215848-demo-review.js
+++ backend/db/seeders/20221023215848-demo-review.js
@@ -10,61 +10,61 @@ module.exports = {
         {
           spotId: 1,
           userId: 2,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 1,
           userId: 3,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 1,
           userId: 10,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 1,
           userId: 15,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 1,
           userId: 4,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 1,
           userId: 5,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 1,
           userId: 14,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 1,
           userId: 12,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 1,
           userId: 9,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 1,
           userId: 8,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
 
@@ -73,61 +73,61 @@ module.exports = {
         {
           spotId: 2,
           userId: 1,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 2,
           userId: 3,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 2,
           userId: 4,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 2,
           userId: 5,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 2,
           userId: 6,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 2,
           userId: 7,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 2,
           userId: 8,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 2,
           userId: 9,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 2,
           userId: 10,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 2,
           userId: 11,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         /* ------------------------------------------------------------------------------ */
@@ -135,61 +135,61 @@ module.exports = {
         {
           spotId: 3,
           userId: 1,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 3,
           userId: 2,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 3,
           userId: 4,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 3,
           userId: 5,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 3,
           userId: 6,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 3,
           userId: 7,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 3,
           userId: 8,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 3,
           userId: 9,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 2
         },
         {
           spotId: 3,
           userId: 10,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 2
         },
         {
           spotId: 3,
           userId: 11,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
 
@@ -198,61 +198,61 @@ module.exports = {
         {
           spotId: 4,
           userId: 1,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 4,
           userId: 2,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 4,
           userId: 3,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 4,
           userId: 5,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 4,
           userId: 6,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 4,
           userId: 7,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 4,
           userId: 8,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 4
         },
         {
           spotId: 4,
           userId: 9,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 2
         },
         {
           spotId: 4,
           userId: 10,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 4,
           userId: 11,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
 
@@ -261,61 +261,61 @@ module.exports = {
         {
           spotId: 5,
           userId: 1,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 5,
           userId: 2,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 5,
           userId: 6,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 5,
           userId: 4,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 5,
           userId: 3,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 5,
           userId: 7,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 5,
           userId: 8,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
         {
           spotId: 5,
           userId: 9,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 2
         },
         {
           spotId: 5,
           userId: 10,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 3
         },
         {
           spotId: 5,
           userId: 11,
-          review: faker.lorem.paragraph(),
+          review: faker.lorem.words(10),
           stars: 5
         },
       ],
