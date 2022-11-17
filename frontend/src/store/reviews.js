c95f157fe9525f76da23a374caba05682dcb66a8
Gray Nance <graynance@gmail.com>
2022-11-17 07:52:40 -0800

    getUserReviews && deleteReview && updateReview thunk

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -19,7 +19,6 @@ export const setReviews = reviews => {
 };
 
 export const createReview = review => {
-  console.log('DATA IN ACTION/ACTION CREATOR', review);
   return {
     type: CREATE_REVIEW,
     review
@@ -41,6 +40,7 @@ export const removeReview = reviewId => {
 };
 
 export const setCurrentUserReviews = currentUserReviews => {
+  console.log('DATA IN ACTION/ACTION CREATOR', currentUserReviews);
   return {
     type: CURRENT_USER_REVIEWS,
     currentUserReviews
@@ -62,6 +62,18 @@ export const getReviews = spotId => async dispatch => {
   return res;
 };
 
+export const getUserReviews = () => async dispatch => {
+  const res = await csrfFetch(`/api/reviews/current`);
+
+  if (res.ok) {
+    const data = await res.json();
+    console.log('DATA IN THUNK/THUNK CREATOR --------->', data);
+    dispatch(setCurrentUserReviews(normalizeArray(data.Reviews)));
+    return res;
+  }
+  return res;
+};
+
 export const postReview = (spotId, payload) => async dispatch => {
   const res = await csrfFetch(`/api/spots/${spotId}/reviews`, {
     method: 'POST',
@@ -70,8 +82,33 @@ export const postReview = (spotId, payload) => async dispatch => {
 
   if (res.ok) {
     const data = await res.json();
-    console.log('DATA IN THUNK/THUNK CREATOR --------->', data);
     dispatch(createReview(data));
+    return res;
+  }
+  return res;
+};
+
+export const putReview = (reviewId, payload) => async dispatch => {
+  const res = await csrfFetch(`/api/reviews/${reviewId}`, {
+    method: 'PUT',
+    body: JSON.stringify(payload)
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(updateReview(data));
+    return res;
+  }
+  return res;
+};
+
+export const deleteReview = reviewId => async dispatch => {
+  const res = await csrfFetch(`api/reviews/${reviewId}`, {
+    method: 'DELETE'
+  });
+
+  if (res.ok) {
+    dispatch(removeReview(reviewId));
     return res;
   }
   return res;
@@ -88,8 +125,17 @@ const reviewsReducer = (state = initialState, action) => {
     case SET_SPOT_REVIEWS: {
       return { ...state, spotReviews: { ...action.reviews } };
     }
+    case CURRENT_USER_REVIEWS: {
+      return { ...state, currentUserReviews: { ...action.currentUserReviews } };
+    }
     case CREATE_REVIEW: {
       return { ...state, spotReviews: { ...state.spotReviews, [action.review.id]: action.review } };
+    }
+    case REMOVE_REVIEW: {
+      const newState = { ...state };
+      if (newState.spotReviews[action.reviewId]) delete newState.spotReviews[action.reviewId];
+      if (newState.currentUserReviews[action.reviewId]) delete newState.currentUserReviews[action.reviewId];
+      return newState;
     }
     default:
       return state;
